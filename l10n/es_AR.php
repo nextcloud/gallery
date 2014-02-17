171e6c1da12b88f66e6f41ec03597543be1dc0f8
Robin Appelman <icewind@owncloud.com>
2014-02-17 21:37:13 +0100

    move the pictures app to it's own repo

---
+++ l10n/es_AR.php
@@ -0,0 +1,9 @@
+<?php $TRANSLATIONS = array(
+"Pictures" => "Imágenes",
+"Shared by" => "Compartido por",
+"Picture view" => "Vista de imágen",
+"Error loading slideshow template" => "Error cargando plantilla de presentación.",
+"Share" => "Compartir",
+"The \"Image Viewer\" application also need to be enabled to use this application." => "La aplicación \"Visor de imágenes\" también debe ser activada para poder usar esta aplicación.",
+"File list" => "Lista de archivos"
+);
