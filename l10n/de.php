73784afdad7cd926890930f54055aab8b5de309c
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2014-03-12 02:22:05 -0400

    [tx-robot] updated from transifex

--- l10n/de.php
+++ l10n/de.php
@@ -4,6 +4,6 @@
 "Picture view" => "Bildansicht",
 "Error loading slideshow template" => "Fehler beim Laden der Slideshow-Vorlage",
 "Share" => "Teilen",
-"shared by %s" => "Freigegeben von %s",
+"shared by %s" => "Geteilt von %s",
 "File list" => "Dateiliste"
 );
