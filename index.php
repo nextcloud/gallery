f74071e83abab214be389f9e7cebef6050201045
Jan-Christoph Borchardt <hey@jancborchardt.net>
2014-03-14 10:08:12 +0100

    mobile: use also for logged-in pictures app. Blocked by core work still

--- index.php
+++ index.php
@@ -10,9 +10,10 @@ OCP\User::checkLoggedIn();
 OCP\App::checkAppEnabled('gallery');
 OCP\App::setActiveNavigationEntry('gallery_index');
 
+OCP\Util::addStyle('gallery', 'styles');
+OCP\Util::addStyle('gallery', 'mobile');
 OCP\Util::addScript('gallery', 'gallery');
 OCP\Util::addScript('gallery', 'thumbnail');
-OCP\Util::addStyle('gallery', 'styles');
 
 $tmpl = new OCP\Template('gallery', 'index', 'user');
 $tmpl->printPage();
