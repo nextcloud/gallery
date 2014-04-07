c239c7efad5b4df61bac4a68f6cf74ff4636282c
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2014-04-07 02:19:36 -0400

    [tx-robot] updated from transifex

--- l10n/sv.php
+++ l10n/sv.php
@@ -1,6 +1,9 @@
 <?php $TRANSLATIONS = array(
 "Pictures" => "Bilder",
 "Shared by" => "Delad av",
+"Picture view" => "Bild vy",
 "Error loading slideshow template" => "Fel vid laddning av mallen för bildspelet",
-"Share" => "Dela"
+"Share" => "Dela",
+"shared by %s" => "delad av %s",
+"File list" => "Fil lista"
 );
