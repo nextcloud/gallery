6595c1fa65cfad4b7bad2047e99a20de5b15a998
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-04-25 02:18:25 -0400

    [tx-robot] updated from transifex

--- l10n/bg_BG.php
+++ l10n/bg_BG.php
@@ -1,6 +1,9 @@
 <?php $TRANSLATIONS = array(
 "Pictures" => "Снимки",
 "Shared by" => "Споделено от",
+"Picture view" => "Виж снимките",
 "Error loading slideshow template" => "Грешка при зареждане на шаблон за слайдшоу",
-"Share" => "Споделяне"
+"Share" => "Споделяне",
+"shared by %s" => "споделено от %s",
+"File list" => "Списък с файлове"
 );
