cfe3985add7bd70988ea9a325363285cde8c473b
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-05-18 02:18:09 -0400

    [tx-robot] updated from transifex

--- l10n/ur_PK.php
+++ l10n/ur_PK.php
@@ -1,3 +1,9 @@
 <?php $TRANSLATIONS = array(
-"Share" => "تقسیم"
+"Pictures" => "تصاویر",
+"Shared by" => "سے اشتراک شدہ",
+"Picture view" => "منظر تصویر",
+"Error loading slideshow template" => "سلائیڈ شو ٹمپلیٹ لوڈ ہونے میں خرابی",
+"Share" => "تقسیم",
+"shared by %s" => "%s سے اشتراک شدہ",
+"File list" => "فہرست فائل"
 );
