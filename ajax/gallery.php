ea91cdb1763ebc38242a5b87d58eeb461324f130
Robin Appelman <icewind@owncloud.com>
2014-05-19 13:26:47 +0200

    fix sharing dialog for gallery

--- ajax/gallery.php
+++ ajax/gallery.php
@@ -9,6 +9,8 @@
 OCP\JSON::checkLoggedIn();
 OCP\JSON::checkAppEnabled('gallery');
 
+$gallery = $_GET['gallery'];
+
 $meta = \OC\Files\Filesystem::getFileInfo($gallery);
 $data = array();
 $data['fileid'] = $meta['fileid'];
