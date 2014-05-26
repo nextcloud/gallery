9f8b6c962aca420c0bcfb4fb3f8166204f9dc6aa
Lukas Reschke <lukas@statuscode.ch>
2014-05-26 21:00:04 +0200

    Setup filesystem earlier

--- ajax/thumbnail.php
+++ ajax/thumbnail.php
@@ -15,11 +15,14 @@ if (is_array($linkItem) && isset($linkItem['uid_owner'])) {
 	// seems to be a valid share
 	$rootLinkItem = \OCP\Share::resolveReShare($linkItem);
 	$user = $rootLinkItem['uid_owner'];
-	$fullPath = \OC\Files\Filesystem::getPath($linkItem['file_source']);
-	$img = trim($fullPath . '/' . $img);
+
+	// Setup filesystem
 	OCP\JSON::checkUserExists($user);
 	OC_Util::tearDownFS();
 	OC_Util::setupFS($user);
+
+	$fullPath = \OC\Files\Filesystem::getPath($linkItem['file_source']);
+	$img = trim($fullPath . '/' . $img);
 } else {
 	OCP\JSON::checkLoggedIn();
 	$user = OCP\User::getUser();
