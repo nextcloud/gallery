796ba81a254e55379c3c8415b4ac390a8abbb2e1
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-06-22 02:18:07 -0400

    [tx-robot] updated from transifex

--- l10n/el.php
+++ l10n/el.php
@@ -3,7 +3,7 @@
 "Picture view" => "Προβολή εικονιδίων",
 "Error loading slideshow template" => "Σφάλμα φόρτωσης προτύπου προβολής διαφανειών",
 "Share" => "Διαμοιρασμός",
-"No pictures found! If you upload pictures in the files app, they will be displayed here." => "Δεν βρέθηκαν εικόνες! Εάν ανεβάσετε εικόνες στην εφαρμογή αρχείων, θα εμφανιστούν εδώ.",
+"No pictures found! If you upload pictures in the files app, they will be displayed here." => "Δεν βρέθηκαν εικόνες! Εάν μεταφορτώσετε εικόνες στην εφαρμογή αρχείων, θα εμφανιστούν εδώ.",
 "shared by %s" => "διαμοιράστηκε από %s",
 "File list" => "Λίστα αρχείων"
 );
