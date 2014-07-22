7e5810be7cefcc73ff0c7492aa3f2b78dcca89fc
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-07-22 02:18:07 -0400

    [tx-robot] updated from transifex

--- l10n/nl.php
+++ l10n/nl.php
@@ -4,6 +4,7 @@
 "Error loading slideshow template" => "Fout bij laden diashow sjabloon",
 "Share" => "Delen",
 "No pictures found! If you upload pictures in the files app, they will be displayed here." => "Geen afbeeldingen gevonden! Als u afbeeldingen upload in de bestandsapp, worden ze hier getoond.",
+"Loading..." => "Laden ...",
 "shared by %s" => "gedeeld door %s",
 "File list" => "Bestandenlijst"
 );
