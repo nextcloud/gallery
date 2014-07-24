1a9fc14557ef189791ccd478f40c34d3002fa330
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-07-24 02:18:08 -0400

    [tx-robot] updated from transifex

--- l10n/pt_PT.php
+++ l10n/pt_PT.php
@@ -4,6 +4,7 @@
 "Error loading slideshow template" => "Erro ao carregar o modelo do diapaorama",
 "Share" => "Compartilhar",
 "No pictures found! If you upload pictures in the files app, they will be displayed here." => "Não foi encontrada nenhuma foto! Se enviar fotografias nos ficheiros da \"app\", estes serão visualizados aqui.",
+"Loading..." => "A carregar...",
 "shared by %s" => "compartilhada por %s",
 "File list" => "Lista de Ficheiros"
 );
