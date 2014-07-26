15b4a8935a22d4af17ea4133c477e71db9894b52
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-07-26 02:18:12 -0400

    [tx-robot] updated from transifex

--- l10n/bg_BG.php
+++ l10n/bg_BG.php
@@ -2,7 +2,7 @@
 "Pictures" => "Снимки",
 "Picture view" => "Албум със снимки",
 "Error loading slideshow template" => "Грешка при зареждане на шаблон за слайдшоу.",
-"Share" => "Споделяне",
+"Share" => "Сподели",
 "No pictures found! If you upload pictures in the files app, they will be displayed here." => "Няма открити снимки! Ако качиш снимки в програмата файлове, те ще бъдат показани тук.",
 "Loading..." => "Зареждане...",
 "shared by %s" => "споделено от %s",
--- l10n/eu.php
+++ l10n/eu.php
@@ -4,6 +4,7 @@
 "Error loading slideshow template" => "Errorea aurkezpen txantiloia kargatzean",
 "Share" => "Elkarbanatu",
 "No pictures found! If you upload pictures in the files app, they will be displayed here." => "Ez da argazkirik aurkitu! Argazkiak fitxategi aplikazioan igoz gero, hemen argertuko dira.",
+"Loading..." => "Kargatzen...",
 "shared by %s" => "%sk partekatuta",
 "File list" => "Fitxategi zerrenda"
 );
