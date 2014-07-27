169db2bda32dc3afa9fa1b6a7baae0c722b6b8bd
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-07-27 02:18:05 -0400

    [tx-robot] updated from transifex

--- l10n/hu_HU.php
+++ l10n/hu_HU.php
@@ -3,6 +3,7 @@
 "Picture view" => "Képnézet",
 "Error loading slideshow template" => "Hiba történt a diasor sablonjának betöltésekor",
 "Share" => "Megosztás",
+"Loading..." => "Betöltés...",
 "shared by %s" => "Megosztotta: %s",
 "File list" => "Fájllista"
 );
