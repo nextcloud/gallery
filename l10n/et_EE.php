e68180aa19784364ab7acb63e9355e2ffcf479b1
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-07-28 02:18:05 -0400

    [tx-robot] updated from transifex

--- l10n/et_EE.php
+++ l10n/et_EE.php
@@ -4,6 +4,7 @@
 "Error loading slideshow template" => "Viga slaidiseansi kujunduse valimisel",
 "Share" => "Jaga",
 "No pictures found! If you upload pictures in the files app, they will be displayed here." => "Pilte ei leitud! Kui sa laadid üles pilte failirakendiga, siis kuvatakse neid siin.",
+"Loading..." => "Laen ...",
 "shared by %s" => "jagas %s",
 "File list" => "Failide nimekiri"
 );
