fd81505e9294b2aaf1f7e9b9817e4cf2ae9d83a1
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-08-26 02:18:15 -0400

    [tx-robot] updated from transifex

--- l10n/pl.php
+++ l10n/pl.php
@@ -3,7 +3,7 @@
 "Picture view" => "Widok zdjęć",
 "Error loading slideshow template" => "Błąd podczas ładowania szablonu pokazu slajdów",
 "Share" => "Udostępnij",
-"No pictures found! If you upload pictures in the files app, they will be displayed here." => "Nie znaleziono zdjęć! Gdy wgrasz zdjęcia do plików, wtedy zdjęcia się tu pojawią.",
+"No pictures found! If you upload pictures in the files app, they will be displayed here." => "Nie znaleziono zdjęć! Zdjęcia pojawią się tutaj, gdy wgrasz je do plików.",
 "shared by %s" => "udostępniony przez %s",
 "File list" => "Lista plików"
 );
