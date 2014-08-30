328cf2f190fe0ec0aa26761f71d51be6513d58df
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-08-30 02:18:08 -0400

    [tx-robot] updated from transifex

--- l10n/bn_BD.php
+++ l10n/bn_BD.php
@@ -1,4 +1,9 @@
 <?php $TRANSLATIONS = array(
 "Pictures" => "চিত্রসমূহ",
-"Share" => "ভাগাভাগি কর"
+"Picture view" => "চিত্র দর্শন",
+"Error loading slideshow template" => "স্লাইডশো টেমপ্লেট লোড করতে সমস্যা হচ্ছে",
+"Share" => "ভাগাভাগি কর",
+"No pictures found! If you upload pictures in the files app, they will be displayed here." => "কোন চিত্র পাওয়া গেলনা! ফাইল অ্যাপে চিত্র আপলোড করলে তা এখানে প্রদর্শিত হবে।",
+"shared by %s" => "%s দ্বারা ভাগাভাগিকৃত",
+"File list" => "ফাইল তালিকা"
 );
