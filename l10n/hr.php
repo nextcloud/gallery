154b22efc6e83e6f1f24f590f6dc64c5267ed4dc
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-09-11 02:18:06 -0400

    [tx-robot] updated from transifex

--- l10n/hr.php
+++ l10n/hr.php
@@ -1,4 +1,9 @@
 <?php $TRANSLATIONS = array(
 "Pictures" => "Slike",
-"Share" => "Podijeli"
+"Picture view" => "Prikaz slika",
+"Error loading slideshow template" => "Pogrešan unos predloška za dijaprojekciju",
+"Share" => "Zajednički resurs (za raspodjelu)",
+"No pictures found! If you upload pictures in the files app, they will be displayed here." => "Nikakve slike nisu nađene! Ako slike učitate u aplikaciju \"Datoteke\", one će biti prikazane ovdje.",
+"shared by %s" => "Podijeljeno od strane %s",
+"File list" => "Popis datoteka"
 );
