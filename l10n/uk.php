18186339650d53d91fa5c2a934b4be0ec420ebf3
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-10-20 02:18:09 -0400

    [tx-robot] updated from transifex

--- l10n/uk.php
+++ l10n/uk.php
@@ -3,5 +3,7 @@
 "Picture view" => "Перегляд Зображення",
 "Error loading slideshow template" => "Помилка завантаження шаблона слайд-шоу",
 "Share" => "Поділитися",
+"No pictures found! If you upload pictures in the files app, they will be displayed here." => "Зображення не знайдено! Якщо ви завантажуєте зображення в додатку файли, вони будуть відображатися тут.",
+"shared by %s" => "поділяють %s",
 "File list" => "Список файлів"
 );
