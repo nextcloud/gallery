71bc46ca51bde141994ac8e35fd5ad42a591a854
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-10-23 02:18:16 -0400

    [tx-robot] updated from transifex

--- l10n/id.php
+++ l10n/id.php
@@ -3,5 +3,7 @@
 "Picture view" => "Tampilan gambar",
 "Error loading slideshow template" => "Kesalahan saat memuat templat slideshow",
 "Share" => "Bagikan",
+"No pictures found! If you upload pictures in the files app, they will be displayed here." => "Tidak ada gambar yang ditemukan! Jika Anda mengunggah gambar ke aplikasi berkas. mereka akan ditampilkan disini.",
+"shared by %s" => "dibagikan oleh %s",
 "File list" => "Daftar berkas"
 );
