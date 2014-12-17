a8ee2af5e9b7980b28512dd8d8467f5a36cedad2
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-12-17 02:18:15 -0500

    [tx-robot] updated from transifex

--- l10n/fr.js
+++ l10n/fr.js
@@ -5,7 +5,7 @@ OC.L10N.register(
     "Picture view" : "Visualisation de l'image",
     "Error loading slideshow template" : "Erreur lors du téléchargement du modèle de diaporama ",
     "Share" : "Partager",
-    "No pictures found! If you upload pictures in the files app, they will be displayed here." : "Aucune image trouvée ! Si vous téléchargez des images dans l'application fichiers, elles seront affichées ici.",
+    "No pictures found! If you upload pictures in the files app, they will be displayed here." : "Aucune image trouvée ! Si vous ajoutez des images dans l'application Fichiers, elles seront affichées ici.",
     "shared by %s" : "partagé par %s",
     "File list" : "Liste des fichiers"
 },
