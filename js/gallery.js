nextcloud/gallery

commit f9d952db2222e7aee80110fad59ff0bf32e888f2

show loading spinner instead of static 'Loading' text

js/gallery.js
@@ -206,13 +206,13 @@ Gallery.showEmpty = function () {
 Gallery.showLoading = function () {
 	$('#emptycontent').addClass('hidden');
 	$('#controls').removeClass('hidden');
-	$('#loading').removeClass('hidden');
+	$('#content').addClass('icon-loading');
 };
 
 Gallery.showNormal = function () {
 	$('#emptycontent').addClass('hidden');
 	$('#controls').removeClass('hidden');
-	$('#loading').addClass('hidden');
+	$('#content').removeClass('icon-loading');
 };
 
 Gallery.slideShow = function (images, startImage, autoPlay) {

templates/index.php
@@ -9,6 +9,5 @@
 <div id="gallery" class="hascontrols"></div>
 
 <div id="emptycontent" class="hidden"><?php p($l->t("No pictures found! If you upload pictures in the files app, they will be displayed here.")); ?></div>
-<div id="loading" class="hidden"><?php p($l->t("Loading...")); ?></div>
 
 <input type="hidden" name="allowShareWithLink" id="allowShareWithLink" value="yes" />

commit 8f656bb6275a8d87ed3b34fe6a5e5aed8940791e

gallery: don't display a spinner forever if empty
f9d952d broke this - it adjusted showLoading and showNormal
for the spinner but forgot to adjust showEmpty, so when the
gallery is empty, you see a spinner forever.

js/gallery.js
@@ -207,7 +207,7 @@ Gallery.view.pushBreadCrumb = function (text, path) {
 Gallery.showEmpty = function () {
 	$('#controls').addClass('hidden');
 	$('#emptycontent').removeClass('hidden');
-	$('#loading').addClass('hidden');
+	$('#content').removeClass('icon-loading');
 };
 
 Gallery.showLoading = function () {