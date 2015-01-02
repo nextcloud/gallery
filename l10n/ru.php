d27c18edda92882565a913e4fdff7daa2f1078a5
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-01-02 02:18:16 -0500

    [tx-robot] updated from transifex

--- l10n/ru.php
+++ l10n/ru.php
@@ -1,6 +1,6 @@
 <?php
 $TRANSLATIONS = array(
-"Pictures" => "Рисунки",
+"Pictures" => "Изображения",
 "Picture view" => "Просмотр картинки",
 "Next" => "Следующий",
 "Play" => "Проиграть",
@@ -10,7 +10,7 @@ $TRANSLATIONS = array(
 "Error loading slideshow template" => "Ошибка при загрузке шаблона слайд-шоу",
 "Share" => "Открыть доступ",
 "No pictures found! If you upload pictures in the files app, they will be displayed here." => "Изображения не найдены! Если вы загрузите изображения, то они будут отображены здесь.",
-"shared by %s" => "Поделиться с %s",
+"shared by %s" => "Опубликовано %s",
 "File list" => "Список файлов"
 );
 $PLURAL_FORMS = "nplurals=3; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2);";
