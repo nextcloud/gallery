607a50771355604de5f1ce12344d64daede13860
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-01-17 02:18:22 -0500

    [tx-robot] updated from transifex

--- l10n/ko.js
+++ l10n/ko.js
@@ -10,6 +10,8 @@ OC.L10N.register(
     "Close" : "닫기",
     "Error loading slideshow template" : "슬라이드 쇼 템플릿을 불러오는 중 오류 발생",
     "Share" : "공유",
+    "No pictures found! If you upload pictures in the files app, they will be displayed here." : "사진이 없습니다! 파일 앱에서 올린 사진이 여기에 표시됩니다.",
+    "shared by %s" : "%s 님이 공유함",
     "File list" : "파일 목록"
 },
 "nplurals=1; plural=0;");
