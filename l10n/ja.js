5f53a8280f62309d576e62d080d7478caf0ed962
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-02-07 02:18:21 -0500

    [tx-robot] updated from transifex

--- l10n/ja.js
+++ l10n/ja.js
@@ -1,8 +1,8 @@
 OC.L10N.register(
     "gallery",
     {
-    "Pictures" : "ピクチャ",
-    "Picture view" : "ピクチャビュー",
+    "Pictures" : "画像",
+    "Picture view" : "ピクチャービュー",
     "Next" : "次",
     "Play" : "再生",
     "Pause" : "一時停止",
@@ -10,7 +10,7 @@ OC.L10N.register(
     "Close" : "閉じる",
     "Error loading slideshow template" : "スライドショーのテンプレートの読み込みエラー",
     "Share" : "共有",
-    "No pictures found! If you upload pictures in the files app, they will be displayed here." : "写真は見つかりませんでした！ファイルアプリでアップロードした写真は、ここに表示されます。",
+    "No pictures found! If you upload pictures in the files app, they will be displayed here." : "画像が見つかりませんでした！ファイルアプリでアップロードした画像は、ここに表示されます。",
     "shared by %s" : "%s で共有中",
     "File list" : "ファイルリスト"
 },
