2a8282218fe381f99ae778dca70382c0bc228d95
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-02-09 02:18:20 -0500

    [tx-robot] updated from transifex

---
+++ l10n/lv.js
@@ -0,0 +1,17 @@
+OC.L10N.register(
+    "gallery",
+    {
+    "Pictures" : "Bildes",
+    "Picture view" : "Attēla skats",
+    "Next" : "Nākamā",
+    "Play" : "Atskaņot",
+    "Pause" : "Pauzēt",
+    "Previous" : "Iepriekšējā",
+    "Close" : "Aizvērt",
+    "Error loading slideshow template" : "Kļūda ielādējot slīdrādes veidolu",
+    "Share" : "Dalīties",
+    "No pictures found! If you upload pictures in the files app, they will be displayed here." : "Nav atrasts neviens attēls! Ja caur failu lietotni augšupielādējat attēlus, tie būs redzami šeit.",
+    "shared by %s" : "%s dalījās",
+    "File list" : "Failu saraksts"
+},
+"nplurals=3; plural=(n%10==1 && n%100!=11 ? 0 : n != 0 ? 1 : 2);");
--- l10n/lv.php
+++ l10n/lv.php
@@ -1,7 +1,16 @@
 <?php
 $TRANSLATIONS = array(
 "Pictures" => "Bildes",
+"Picture view" => "Attēla skats",
+"Next" => "Nākamā",
+"Play" => "Atskaņot",
+"Pause" => "Pauzēt",
+"Previous" => "Iepriekšējā",
+"Close" => "Aizvērt",
 "Error loading slideshow template" => "Kļūda ielādējot slīdrādes veidolu",
-"Share" => "Dalīties"
+"Share" => "Dalīties",
+"No pictures found! If you upload pictures in the files app, they will be displayed here." => "Nav atrasts neviens attēls! Ja caur failu lietotni augšupielādējat attēlus, tie būs redzami šeit.",
+"shared by %s" => "%s dalījās",
+"File list" => "Failu saraksts"
 );
 $PLURAL_FORMS = "nplurals=3; plural=(n%10==1 && n%100!=11 ? 0 : n != 0 ? 1 : 2);";
