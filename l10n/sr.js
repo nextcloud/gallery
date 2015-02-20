738a303d182dedacd412c492b390561978884840
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-02-20 02:18:27 -0500

    [tx-robot] updated from transifex

---
+++ l10n/sr.js
@@ -0,0 +1,17 @@
+OC.L10N.register(
+    "gallery",
+    {
+    "Pictures" : "Слике",
+    "Picture view" : "Приказ слике",
+    "Next" : "Следећа",
+    "Play" : "Пусти",
+    "Pause" : "Паузирај",
+    "Previous" : "Претходна",
+    "Close" : "Затвори",
+    "Error loading slideshow template" : "Грешка при учитавању обрасца покретног приказа",
+    "Share" : "Дели",
+    "No pictures found! If you upload pictures in the files app, they will be displayed here." : "Нема слика! Ако отпремите слике у апликацију фајлова, биће приказане овде.",
+    "shared by %s" : "поделио %s",
+    "File list" : "Списак фајлова"
+},
+"nplurals=3; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2);");
--- l10n/sr.php
+++ l10n/sr.php
@@ -1,7 +1,16 @@
 <?php
 $TRANSLATIONS = array(
 "Pictures" => "Слике",
+"Picture view" => "Приказ слике",
+"Next" => "Следећа",
+"Play" => "Пусти",
+"Pause" => "Паузирај",
+"Previous" => "Претходна",
+"Close" => "Затвори",
 "Error loading slideshow template" => "Грешка при учитавању обрасца покретног приказа",
-"Share" => "Дели"
+"Share" => "Дели",
+"No pictures found! If you upload pictures in the files app, they will be displayed here." => "Нема слика! Ако отпремите слике у апликацију фајлова, биће приказане овде.",
+"shared by %s" => "поделио %s",
+"File list" => "Списак фајлова"
 );
 $PLURAL_FORMS = "nplurals=3; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2);";
