470605ba4b0a773c0d035a445ac88408ba8fdfa2
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-02-21 02:18:19 -0500

    [tx-robot] updated from transifex

--- l10n/sr.js
+++ l10n/sr.js
@@ -9,9 +9,9 @@ OC.L10N.register(
     "Previous" : "Претходна",
     "Close" : "Затвори",
     "Error loading slideshow template" : "Грешка при учитавању обрасца покретног приказа",
-    "Share" : "Дели",
-    "No pictures found! If you upload pictures in the files app, they will be displayed here." : "Нема слика! Ако отпремите слике у апликацију фајлова, биће приказане овде.",
-    "shared by %s" : "поделио %s",
+    "Share" : "Подели",
+    "No pictures found! If you upload pictures in the files app, they will be displayed here." : "Нема слика! Ако отпремите слике у апликацији фајлова, биће приказане овде.",
+    "shared by %s" : "дели %s",
     "File list" : "Списак фајлова"
 },
 "nplurals=3; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2);");
