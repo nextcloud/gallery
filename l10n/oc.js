f8a2ef3834e612464b196f8017571f3fd57fab91
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-06-02 02:18:25 -0400

    [tx-robot] updated from transifex

---
+++ l10n/oc.js
@@ -0,0 +1,17 @@
+OC.L10N.register(
+    "gallery",
+    {
+    "Pictures" : "Imatges",
+    "Picture view" : "Visualizacion de l'imatge",
+    "Next" : "Seguent",
+    "Play" : "Aviar",
+    "Pause" : "Pausa",
+    "Previous" : "Precedent",
+    "Close" : "Tampar",
+    "Error loading slideshow template" : "Error al moment del telecargament del modèl de diaporama ",
+    "Share" : "Partejar",
+    "No pictures found! If you upload pictures in the files app, they will be displayed here." : "Cap d'imatge pas trobat ! Se apondètz d'imatges dins l'aplicacion Fichièrs, seràn afichadas aicí.",
+    "shared by %s" : "partejat per %s",
+    "File list" : "Lista dels fichièrs"
+},
+"nplurals=2; plural=(n > 1);");
