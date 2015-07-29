83074af1c38f3a3c6804de62ef6e4e91f6342b30
Olivier Paroz <github@oparoz.com>
2015-07-29 02:25:19 +0200

    Use a new composer ID to avoid conflicts (cherry picked from commit 4a3e36e)

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit3cfd72445e0ad28db5332442bcda1e66::getLoader();
+return ComposerAutoloaderInit473f5a1120b355647f39b636e46b35f0::getLoader();
