b84508a0b687b91b623c6f81ea99e5fe40daa846
Olivier Paroz <github@oparoz.com>
2015-08-11 02:34:03 +0200

    1st acceptance and api tests

---
+++ tests/api/CreateUserCept.php
@@ -0,0 +1,24 @@
+<?php
+use Codeception\Util\Xml as XmlUtils;
+
+$I = new ApiTester($scenario);
+$I->wantTo('create a user via the provisioning API');
+$baseUrl = '/ocs/v1.php/cloud';
+$I->amHttpAuthenticated('admin', 'admin');
+$I->haveHttpHeader('Content-Type', 'application/x-www-form-urlencoded');
+$I->sendPOST($baseUrl . '/users', ['userid' => 'test', 'password' => 'test']);
+$I->seeResponseCodeIs(200);
+$I->seeResponseIsXml();
+$I->seeXmlResponseIncludes(
+	XmlUtils::toXml(
+		['status'   => 'ok']
+));
+
+// Make sure the user exists
+$I->sendGET($baseUrl . '/users/test');
+$I->seeResponseCodeIs(200);
+$I->seeResponseIsXml();
+$I->seeXmlResponseIncludes(
+	XmlUtils::toXml(
+		['status'   => 'ok']
+));
