3603aa66266c0815d5e98647666a371a70eaf2ca
Olivier Paroz <github@oparoz.com>
2015-08-19 04:13:53 +0200

    More unit tests for the controllers

--- tests/unit/controller/ConfigPublicControllerTest.php
+++ tests/unit/controller/ConfigPublicControllerTest.php
@@ -12,24 +12,22 @@
 
 namespace OCA\Gallery\Controller;
 
-require_once __DIR__ . '/FilesControllerTest.php';
+require_once __DIR__ . '/ConfigControllerTest.php';
+
 /**
- * Class FilesPublicControllerTest
+ * Class ConfigPublicControllerTest
  *
  * @package OCA\Gallery\Controller
  */
-class FilesPublicControllerTest extends FilesControllerTest {
+class ConfigPublicControllerTest extends ConfigControllerTest {
 
 	public function setUp() {
 		parent::setUp();
-		$this->controller = new FilesPublicController(
+		$this->controller = new ConfigPublicController(
 			$this->appName,
 			$this->request,
-			$this->urlGenerator,
-			$this->searchFolderService,
 			$this->configService,
-			$this->searchMediaService,
-			$this->downloadService,
+			$this->previewService,
 			$this->logger
 		);
 	}
