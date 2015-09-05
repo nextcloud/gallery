6b8dd2ad875ac58b93945cbbcaa1995ba14b1d2e
Olivier Paroz <github@oparoz.com>
2015-09-05 03:14:14 +0200

    Add test for navigation entry loading Routes are not loaded, so we just make sure we get an exception (cherry picked from commit 06cab3b)

--- tests/integration/appinfo/AppTest.php
+++ tests/integration/appinfo/AppTest.php
@@ -45,4 +45,14 @@ class AppTest extends GalleryIntegrationTest {
 
 		$this->assertSame('7', $appData['dependencies']['php']['@attributes']['max-version']);
 	}
+
+	/**
+	 * Routes are not loaded any more, so we can't test the navigation entry, but we know there
+	 * will be an exception thrown when trying to load the navigation
+	 *
+	 * @expectedException \Symfony\Component\Routing\Exception\RouteNotFoundException
+	 */
+	public function testNavigationEntry() {
+		$this->assertCount(1, \OC_App::getAppNavigationEntries('gallery'));
+	}
 }
