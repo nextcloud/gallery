f67d0c2d78b4f28347727ca5384948bdb4e88f55
Olivier Paroz <github@oparoz.com>
2015-09-20 23:38:37 +0200

    Added test for text/plain SVGs in getPreview

--- tests/unit/controller/PreviewApiControllerTest.php
+++ tests/unit/controller/PreviewApiControllerTest.php
@@ -14,12 +14,21 @@ namespace OCA\Gallery\Controller;
 
 require_once __DIR__ . '/PreviewControllerTest.php';
 
+use OCP\Files\File;
+
+use OCP\AppFramework\Http;
+
+use OCA\Gallery\Http\ImageResponse;
+
 /**
  * Class PreviewApiControllerTest
  *
  * @package OCA\Gallery\Controller
  */
 class PreviewApiControllerTest extends PreviewControllerTest {
+
+	/** @var PreviewApiController */
+	protected $controller;
 
 	public function setUp() {
 		parent::setUp();
@@ -33,6 +42,47 @@ class PreviewApiControllerTest extends PreviewControllerTest {
 			$this->downloadService,
 			$this->eventSource,
 			$this->logger
+		);
+	}
+
+	/**
+	 * @return array
+	 */
+	public function providesTestDownloadData() {
+		return [
+			[1234, $this->mockSvgFile(1234), true, 'image/svg+xml'],
+			[4567, $this->mockSvgFile(4567), false, 'text/plain']
+		];
+	}
+
+	/**
+	 * @dataProvider providesTestDownloadData
+	 *
+	 * @param int $fileId
+	 * @param File $file
+	 * @param string $nativeSvg
+	 * @param string $expectedMimeType
+	 *
+	 * @internal param string $type
+	 */
+	public function testGetPreviewOfSvg($fileId, $file, $nativeSvg, $expectedMimeType) {
+		$width = 1024;
+		$height = 768;
+
+		/** @type File $file */
+		$preview = $this->mockGetData(
+			$fileId, $file, $width, $height, $keepAspect = true, $animatedPreview = true,
+			$base64Encode = false, $previewRequired = false
+		);
+		$preview['name'] = $file->getName();
+
+		/** @type ImageResponse $response */
+		$response = $this->controller->getPreview($fileId, $width, $height, $nativeSvg);
+
+		$this->assertEquals(Http::STATUS_OK, $response->getStatus());
+
+		$this->assertEquals(
+			$expectedMimeType . '; charset=utf-8', $response->getHeaders()['Content-type']
 		);
 	}
 
