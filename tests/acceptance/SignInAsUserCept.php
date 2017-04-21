e8fce2e8f5e8fd9e258268a9e36760ad88c7f681
Roeland Jago Douma <roeland@famdouma.nl>
2017-04-21 15:20:29 +0200

    Fix the acceptance tests sine we have a fancy new appmenu!
    Signed-off-by: Roeland Jago Douma <roeland@famdouma.nl>

--- tests/acceptance/SignInAsUserCept.php
+++ tests/acceptance/SignInAsUserCept.php
@@ -22,8 +22,6 @@ $loginPage = new Login($I);
 $loginPage->login('admin', 'admin');
 $loginPage->confirmLogin();
 
-
-$I->click('.menutoggle');
-$I->click('Gallery', '#navigation');
+$I->click('Gallery', '#appmenu');
 $I->seeCurrentUrlEquals(GalleryPage::$URL);
 $I->seeElement(['css' => GalleryPage::$contentDiv]);
