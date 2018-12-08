b8aeb239a522de40c273b4d1d75d90683d16d3b7
Tomasz Grobelny <tomasz@grobelny.net>
2018-12-08 23:23:58 +0100

    Convert dodatbase.xml to migration
    Signed-off-by: Tomasz Grobelny <tomasz@grobelny.net>

---
+++ lib/Migration/Version1803Date20181208221653.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OCA\Gallery\Migration;
+
+use Closure;
+use OCP\DB\ISchemaWrapper;
+use OCP\Migration\SimpleMigrationStep;
+use OCP\Migration\IOutput;
+
+/**
+ * Auto-generated migration step: Please modify to your needs!
+ */
+class Version1803Date20181208221653 extends SimpleMigrationStep {
+
+	/**
+	 * @param IOutput $output
+	 * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
+	 * @param array $options
+	 */
+	public function preSchemaChange(IOutput $output, Closure $schemaClosure, array $options) {
+	}
+
+	/**
+	 * @param IOutput $output
+	 * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
+	 * @param array $options
+	 * @return null|ISchemaWrapper
+	 */
+	public function changeSchema(IOutput $output, Closure $schemaClosure, array $options) {
+		/** @var ISchemaWrapper $schema */
+		$schema = $schemaClosure();
+
+		if (!$schema->hasTable('gallery_file_properties')) {
+			$table = $schema->createTable('gallery_file_properties');
+			$table->addColumn('id', 'integer', [
+				'notnull' => true,
+			]);
+			$table->addColumn('modifications', 'string', [
+				'notnull' => false,
+			]);
+		}
+		return $schema;
+	}
+
+	/**
+	 * @param IOutput $output
+	 * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
+	 * @param array $options
+	 */
+	public function postSchemaChange(IOutput $output, Closure $schemaClosure, array $options) {
+	}
+}
